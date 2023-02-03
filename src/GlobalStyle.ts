4a57571ebfa239e35685048d704f94dab1a2bfdd
Ricardo Gava <ricardoagava@gmail.com>
2023-02-03 20:42:25 -0300

    style: added some default style to some elements

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -4,9 +4,33 @@ export const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
       'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+  }
+
+  body {
+    padding-top: 4rem;
+    color: #333;
+    --type-first: Helvetica, Arial, sans-serif;
+    --type-second: 'Spectral', Georgia;
+    font-family: var(--type-first);
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+  }
+
+  button,
+  input {
+    display: block;
+    font-size: 1rem;
+    font-family: var(--type-first);
+    color: #333;
   }
 `
