35f4f0a5b029a2c13dbdab4eead1d3ddb9986068
Ricardo Gava <ricardoagava@gmail.com>
2023-02-03 20:40:33 -0300

    feat: created Layout and Main containers, make routes for Home and Login pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import styled from "styled-components"
+
+import { Route, Routes } from "react-router"
+import { BrowserRouter } from "react-router-dom"
+
+import { Home, Login } from "Pages"
+import { Header, Footer } from "Components"
+
+const Layout = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 0 10px;
+  background-color: tomato;
+`
+
+const Main = styled.main`
+  background-color: green;
+`
+
 export function App() {
   return (
-    <h1>Dogs</h1>
+    <BrowserRouter>
+      <Layout>
+        <Header />
+        <Main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Main>
+        <Footer />
+      </Layout>
+    </BrowserRouter>
   )
 }
