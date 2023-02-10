a83814c169a934d40630dd6c55f84d2a4ac27ec7
Ricardo Gava <ricardoagava@gmail.com>
2023-02-09 23:50:01 -0300

    style: added a global style for titles

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -6,13 +6,12 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     list-style: none;
-    text-decoration: none;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
-
+  
   body {
     padding-top: 4rem;
     color: #333;
@@ -25,7 +24,7 @@ export const GlobalStyle = createGlobalStyle`
     display: block;
     max-width: 100%;
   }
-
+  
   button,
   input {
     display: block;
@@ -33,4 +32,37 @@ export const GlobalStyle = createGlobalStyle`
     font-family: var(--type-first);
     color: #333;
   }
-`
+
+  a {
+    text-decoration: none;
+    color: #333;
+  }
+
+  h1 {
+      font-family: var(--type-second);
+      line-height: 1;
+      font-size: 3rem;
+      margin: 1rem 0;
+      position: relative;
+    }
+
+  h1::after {
+    content: '';
+    display: block;
+    width: 1.5rem;
+    height: 1.5rem;
+    background-color: #fb1;
+    position: absolute;
+    bottom: 5px;
+    left: -5px;
+    border-radius: 0.2rem;
+    z-index: -1;
+  }
+
+  @keyframes animeLeft {
+    to {
+      opacity: 1;
+      transform: initial;
+    }
+  }
+  `
