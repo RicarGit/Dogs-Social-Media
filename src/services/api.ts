5e32cff4052c41cc7ba599feaa1a76c641be9388
Ricardo Gava <ricardoagava@gmail.com>
2023-02-13 00:02:23 -0300

    feat: added USER_POST function in api to send the user data to be registered

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,13 @@
-interface Login {
+interface UserInfo {
   username: string
   password: string
+  email?: string
 }
 
 const API_URL = 'https://dogsapi.origamid.dev/json'
 
 export const api = {
-  TOKEN_POST: (body: Login) => {
+  TOKEN_POST: (body: UserInfo) => {
     return {
       url: `${API_URL}/jwt-auth/v1/token`,
       options: {
@@ -37,6 +38,18 @@ export const api = {
         headers: {
           Authorization: `Bearer ${token}`
         }
+      }
+    }
+  },
+  USER_POST: (body: UserInfo) => {
+    return {
+      url: `${API_URL}/api/user`,
+      options: {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
       }
     }
   }
