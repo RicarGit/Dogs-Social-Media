489b454f7d6ab304e6b51ebbd99c320464de175f
Ricardo Gava <ricardoagava@gmail.com>
2023-03-03 23:50:53 -0300

    feat: adding FeedPhotos component with photo data and display the photos on the screen

--- src/Components/Feed/FeedPhotos/FeedPhotos.styled.ts
+++ src/Components/Feed/FeedPhotos/FeedPhotos.styled.ts
@@ -1,5 +1,17 @@
-import styled from "styled-components"
+import styled from "styled-components/macro"
 
 export const FeedPhotos = styled.div`
+  
+`
 
+export const PhotoList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
+  margin-bottom: 1rem;
+  justify-items: center;
+
+  @media (max-width: 40rem) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `
--- src/Components/Feed/FeedPhotos/FeedPhotos.tsx
+++ src/Components/Feed/FeedPhotos/FeedPhotos.tsx
@@ -1,7 +1,42 @@
-import { FeedPhotosItem } from "./FeedPhotosItem/FeedPhotosItem"
+import * as S from './FeedPhotos.styled'
+import { useEffect, Dispatch, SetStateAction } from "react"
+import { useFetch } from "hooks/useFetch"
+import { PhotoInfo } from 'types/photoInfo'
+import { api } from "services/api"
 
-export const FeedPhotos = () => {
+import { FeedPhotosItem } from "./FeedPhotosItem"
+import { Error } from "Components/Error"
+import { Loading } from "Components/Loading"
+
+interface SetModal {
+  setModalPhoto: Dispatch<SetStateAction<PhotoInfo | null>>
+}
+
+export const FeedPhotos = ({ setModalPhoto }: SetModal) => {
+  const { data, error, loading, request } = useFetch()
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      const { url, options } = api.PHOTOS_GET({ page: 1, total: 12, user: '0' })
+      const { response, json } = await request(url, options)
+    }
+
+    fetchPhotos()
+  }, [request])
+
+  if (error) return <Error error={error} />
+  if (loading) return <Loading />
+  if (!data) return null
+
   return (
-    <FeedPhotosItem />
+    <S.PhotoList className='animeLeft'>
+      {(Array.isArray(data)) && data.map(photo =>
+        <FeedPhotosItem
+          key={photo.id}
+          photoData={photo}
+          setModalPhoto={setModalPhoto}
+        />
+      )}
+    </S.PhotoList>
   )
 }
