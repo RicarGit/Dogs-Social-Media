594a3ec00dbbf28c01e84b25d21deb9b3d67e125
Ricardo Gava <ricardoagava@gmail.com>
2023-03-03 23:46:48 -0300

    feat: adding raw FeedModal component

--- src/Components/Feed/FeedModal/FeedModal.tsx
+++ src/Components/Feed/FeedModal/FeedModal.tsx
@@ -1,5 +1,20 @@
-export const FeedModal = () => {
+import * as S from './FeedModal.styled'
+import { useEffect } from 'react'
+import { useFetch } from 'hooks/useFetch'
+import { PhotoInfo } from 'types/photoInfo'
+import { api } from 'services/api'
+
+export const FeedModal = ({ photo }: PhotoInfo) => {
+  const { data, error, loading, request } = useFetch()
+
+  useEffect(() => {
+    const { url, options } = api.PHOTO_GET(photo.id)
+    request(url, options)
+  }, [photo, request])
+
   return (
-    <div>FeedModal</div>
+    <S.FeedModal>
+      FeedModal
+    </S.FeedModal>
   )
 }
