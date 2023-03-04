3e41f43607e7101f09225c7e7c291432f9b65194
Ricardo Gava <ricardoagava@gmail.com>
2023-03-03 23:39:27 -0300

    feat: add PhotoInfo interface to his own folder and file

---
+++ src/types/photoInfo.ts
@@ -0,0 +1,13 @@
+export interface PhotoInfo {
+  photo: {
+    acessos: string
+    author: string
+    date: string
+    id: number
+    idade: string
+    peso: string
+    src: string
+    title: string
+    total_comments: string
+  }
+}
