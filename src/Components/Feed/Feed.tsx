cd7c92c65a71464de572993a6f0bbe8705d53986
Ricardo Gava <ricardoagava@gmail.com>
2023-03-06 23:55:33 -0300

    feat: add a state to the modal

--- src/Components/Feed/Feed.tsx
+++ src/Components/Feed/Feed.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react"
 import { FeedModal } from "./FeedModal"
 import { FeedPhotos } from "./FeedPhotos"
+import { PhotoInfo } from "types/photoInfo"
 
 export const Feed = () => {
+  const [modalPhoto, setModalPhoto] = useState<PhotoInfo | null>(null)
+
   return (
     <>
-      <FeedModal />
-      <FeedPhotos />
+      {modalPhoto && <FeedModal photo={modalPhoto.photo} />}
+      <FeedPhotos setModalPhoto={setModalPhoto} />
     </>
   )
 }
