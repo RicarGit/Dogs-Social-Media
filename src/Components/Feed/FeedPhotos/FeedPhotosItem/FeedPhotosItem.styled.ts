3876381019f0f99d901d952ee165055323b5febf
Ricardo Gava <ricardoagava@gmail.com>
2023-03-10 13:45:59 -0300

    feat: now FeedPhotosItem displays the user photos on the Feed's page

--- src/Components/Feed/FeedPhotos/FeedPhotosItem/FeedPhotosItem.styled.ts
+++ src/Components/Feed/FeedPhotos/FeedPhotosItem/FeedPhotosItem.styled.ts
@@ -1,5 +1,49 @@
 import styled from "styled-components/macro"
+import views from 'assets/visualizacao.svg'
 
-export const FeedPhotosItem = styled.div`
+export const FeedPhotosItem = styled.li`
+  display: grid;
+  border-radius: .2rem;
+  overflow: hidden;
+  cursor: pointer;
 
+  &:nth-child(2) {
+    grid-column: 2/4;
+    grid-row: span 2;
+  }
+
+  &:hover span {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  @media (max-width: 40rem) {
+    &:nth-child(2) {
+      grid-column: initial;
+      grid-row: initial;
+    }
+  }
+`
+
+export const PhotoItem = styled.img`
+  grid-area: 1/1;
+`
+
+export const PhotoViews = styled.span`
+  grid-area: 1/1;
+  background-color: rgba(0, 0, 0, .3);
+  color: #fff;
+  font-size: 1rem;
+  display: none;
+
+  &::before {
+    content: '';
+    width: 16px;
+    height: 10px;
+    display: inline-block;
+    margin-right: .25rem;
+    margin-top: .2rem;
+    background: url(${views}) no-repeat;
+  }
 `
--- src/Components/Feed/FeedPhotos/FeedPhotosItem/FeedPhotosItem.tsx
+++ src/Components/Feed/FeedPhotos/FeedPhotosItem/FeedPhotosItem.tsx
@@ -1,5 +1,20 @@
-export const FeedPhotosItem = () => {
+import * as S from './FeedPhotosItem.styled'
+import { PhotoInfo } from 'types/photoInfo'
+import { SetModalProps } from 'types/setModal'
+
+interface SetModalPhotoData extends SetModalProps {
+  photo: PhotoInfo
+}
+
+export const FeedPhotosItem = ({ photo, setModal }: SetModalPhotoData) => {
+  const handleModalPhoto = () => {
+    setModal(photo)
+  }
+
   return (
-    <div>FeedPhotosItem</div>
+    <S.FeedPhotosItem onClick={handleModalPhoto}>
+      <S.PhotoItem src={photo.src} alt={photo.title} />
+      <S.PhotoViews>{photo.acessos}</S.PhotoViews>
+    </S.FeedPhotosItem>
   )
 }
