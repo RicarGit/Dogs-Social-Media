c4a09646770a951d6c5de67d7b2fcbaa0869b1ee
Ricardo Gava <ricardoagava@gmail.com>
2023-03-10 13:30:02 -0300

    refactor: abstracting the PhotoInfo interface into 2 interfaces

--- src/types/photoInfo.ts
+++ src/types/photoInfo.ts
@@ -1,13 +1,15 @@
 export interface PhotoInfo {
-  photo: {
-    acessos: string
-    author: string
-    date: string
-    id: number
-    idade: string
-    peso: string
-    src: string
-    title: string
-    total_comments: string
-  }
+  acessos: string
+  author: string
+  date: string
+  id: number
+  idade: string
+  peso: string
+  src: string
+  title: string
+  total_comments: string
+}
+
+export interface PhotoData {
+  photo: PhotoInfo
 }
