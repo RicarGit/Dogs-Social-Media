315156bbb929d80e29741381224227d0976c9cec
Ricardo Gava <ricardoagava@gmail.com>
2023-03-14 12:47:07 -0300

    feat: showing possible errors, loading while fetching data and PhotoContent when data exists

--- src/Components/Feed/FeedModal/FeedModal.tsx
+++ src/Components/Feed/FeedModal/FeedModal.tsx
@@ -3,6 +3,9 @@ import { useEffect } from 'react'
 import { useFetch } from 'hooks/useFetch'
 import { PhotoData } from 'types/photoInfo'
 import { api } from 'services/api'
+import { Error } from 'Components/Error'
+import { Loading } from 'Components/Loading'
+import { PhotoContent } from 'Components/Photo/PhotoContent'
 
 export const FeedModal = ({ photo }: PhotoData) => {
   const { data, error, loading, request } = useFetch()
@@ -14,7 +17,9 @@ export const FeedModal = ({ photo }: PhotoData) => {
 
   return (
     <S.FeedModal>
-      FeedModal
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && ('comments' in data) && <PhotoContent commentsData={data} />}
     </S.FeedModal>
   )
 }
