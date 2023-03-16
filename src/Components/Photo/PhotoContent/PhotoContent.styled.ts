2dc05b60a68fbff46d2ae56485de7cb89aeed096
Ricardo Gava <ricardoagava@gmail.com>
2023-03-16 00:09:35 -0300

    style: stylized some areas of PhotoContent component

--- src/Components/Photo/PhotoContent/PhotoContent.styled.ts
+++ src/Components/Photo/PhotoContent/PhotoContent.styled.ts
@@ -1,11 +1,35 @@
 import styled from "styled-components/macro"
+import views from 'assets/visualizacao-black.svg'
 
 export const PhotoContent = styled.div`
+  height: 36rem;
+  border-radius: .2rem;
+  background-color: #fff;
+  display: grid;
+  grid-template-columns: 36rem 20rem;
+  grid-template-rows: auto 1fr auto;
+  overflow: hidden;
+  opacity: 0;
+  transform: scale(.8);
+  animation: scaleUp .3s forwards;
 
+  @keyframes scaleUp {
+    to {
+      opacity: initial;
+      transform: initial;
+    }
+  }
+
+  @media (max-width: 64rem) {
+    height: auto;
+    max-height: calc(100vh - 4rem);
+    overflow-y: auto;
+    grid-template-columns: minmax(20rem, 40rem);
+  }
 `
 
 export const PhotoImage = styled.img`
-
+  grid-row: 1/4;
 `
 
 export const DetailsContainer = styled.div`
@@ -13,13 +37,30 @@ export const DetailsContainer = styled.div`
 `
 
 export const Details = styled.div`
+  padding: 2rem 2rem 0 2rem;
 `
 
 export const Author = styled.p`
+  opacity: .5;
+  margin-bottom: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  a:hover {
+    text-decoration: underline;
+  }
 `
 
-export const Accesses = styled.span`
-
+export const Views = styled.span`
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 16px;
+    height: 10px;
+    margin-right: .5rem;
+    background: url(${views});
+  }
 `
 
 export const PostTitle = styled.h1`
@@ -27,9 +68,25 @@ export const PostTitle = styled.h1`
 `
 
 export const AttributesList = styled.ul`
-
+  display: flex;
+  font-size: 1.125rem;
+  font-weight: bold;
+  margin-top: 1rem;
+  margin-bottom: 2rem;
 `
 
 export const Attribute = styled.li`
+  margin-right: 2rem;
 
+  &::before {
+    content: '';
+    display: inline-block;
+    height: 20px;
+    margin-right: .5rem;
+    position: relative;
+    top: 3px;
+    width: 2px;
+    background-color: #333;
+    margin-top: 5px;
+  }
 `
