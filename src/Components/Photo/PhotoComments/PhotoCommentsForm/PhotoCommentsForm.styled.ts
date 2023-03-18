4068fb6430ecef4130d059bb0cc635562d2d44dd
Ricardo Gava <ricardoagava@gmail.com>
2023-03-17 23:56:17 -0300

    feat: add PhotoCommentsForm component

---
+++ src/Components/Photo/PhotoComments/PhotoCommentsForm/PhotoCommentsForm.styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/macro"
+
+export const PhotoCommentsForm = styled.form`
+
+`
+
+export const CommentsArea = styled.textarea`
+
+`
+
+export const CommentsAreaButton = styled.button`
+
+`
---
+++ src/Components/Photo/PhotoComments/PhotoCommentsForm/PhotoCommentsForm.tsx
@@ -0,0 +1,46 @@
+import * as S from './PhotoCommentsForm.styled'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { ReactComponent as Enviar } from 'assets/enviar.svg'
+import { useFetch } from 'hooks/useFetch'
+import { api } from 'services/api'
+import { getStorageToken } from 'contexts/UserContext'
+
+interface CommentsFormId {
+  id: number
+}
+
+export const PhotoCommentsForm = ({ id }: CommentsFormId) => {
+  const [comment, setComment] = useState('')
+  const { request, error } = useFetch()
+
+  const setCommentary = ({ target }: ChangeEvent<HTMLTextAreaElement>) => {
+    const comment = target.value
+    setComment(comment)
+  }
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const token = getStorageToken()
+
+    if (token) {
+      const { url, options } = api.COMMENT_POST(id, { comment }, token)
+      await request(url, options)
+    }
+  }
+
+  return (
+    <S.PhotoCommentsForm onSubmit={handleSubmit}>
+      <S.CommentsArea
+        id='comment'
+        name='comment'
+        placeholder='Comente aqui...'
+        value={comment}
+        onChange={setCommentary}
+      />
+
+      <S.CommentsAreaButton>
+        <Enviar />
+      </S.CommentsAreaButton>
+    </S.PhotoCommentsForm>
+  )
+}
