bd4468299ead498234eaaf92340f3d34d3b72d68
Ricardo Gava <ricardoagava@gmail.com>
2023-03-22 12:45:27 -0300

    feat: photoComments component is now displaying the users comments
    also when comments area is full of comments an scroll will shown and all new comments will be thrown
    at the end of the list

--- src/Components/Photo/PhotoComments/PhotoComments.styled.ts
+++ src/Components/Photo/PhotoComments/PhotoComments.styled.ts
@@ -1,5 +1,20 @@
 import styled from "styled-components/macro"
 
 export const PhotoComments = styled.ul`
+  overflow-y: auto;
+  word-break: break-all;
+  padding: 0 2rem;
+`
+
+export const PhotoComment = styled.li`
+  margin-bottom: .5rem;
+  line-height: 1.2;
+`
+
+export const CommentAuthor = styled.b`
+
+`
+
+export const CommentContent = styled.span`
 
 `
--- src/Components/Photo/PhotoComments/PhotoComments.tsx
+++ src/Components/Photo/PhotoComments/PhotoComments.tsx
@@ -1,6 +1,9 @@
-import { PhotoCommentsType } from 'types/photoComments'
 import * as S from './PhotoComments.styled'
+import { useEffect, useRef, useState } from 'react'
 import { CommentType } from 'types/photoComments'
+import { useUserContext } from 'contexts/UserContext'
+
+import { PhotoCommentsForm } from './PhotoCommentsForm'
 
 interface PhotoCommentsInfo {
   id: number
@@ -8,7 +11,34 @@ interface PhotoCommentsInfo {
 }
 
 export const PhotoComments = ({ id, comments }: PhotoCommentsInfo) => {
+  const [commentaries, setCommentaries] = useState(comments)
+  const commentSection = useRef<HTMLUListElement>(null)
+  const { login } = useUserContext()
+
+  useEffect(() => {
+    const commentListElement = commentSection.current
+
+    if (commentListElement) {
+      commentListElement.scrollTop = commentListElement.scrollHeight
+    }
+  }, [commentaries])
+
+  const updateCommentaries = (comments: CommentType) => {
+    setCommentaries(prevState => [...prevState, comments])
+  }
+
   return (
-    <div>PhotoComments</div>
+    <>
+      <S.PhotoComments ref={commentSection}>
+        {commentaries.map(comment => (
+          <S.PhotoComment key={comment.comment_ID}>
+            <S.CommentAuthor>{comment.comment_author}: </S.CommentAuthor>
+            <S.CommentContent>{comment.comment_content}</S.CommentContent>
+          </S.PhotoComment>
+        ))}
+      </S.PhotoComments>
+
+      {login && <PhotoCommentsForm id={id} updateCommentaries={updateCommentaries} />}
+    </>
   )
 }
