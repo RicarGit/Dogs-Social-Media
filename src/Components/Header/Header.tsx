e9287e7496011ea1b4fb530e08043279dd02a85b
Ricardo Gava <ricardoagava@gmail.com>
2023-03-23 23:10:03 -0300

    feat: when user logged in the user name will shown in header, when not, login / create will shown

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,10 @@
 import * as S from './Header.styled'
 import { Link } from 'react-router-dom'
 import { useUserContext } from 'contexts/UserContext'
-
 import { ReactComponent as Dogs } from 'assets/dogs.svg'
 
 export const Header = () => {
-  const { data, userLogout } = useUserContext()
+  const { data, login } = useUserContext()
 
   return (
     <S.Header>
@@ -13,7 +12,10 @@ export const Header = () => {
         <Link className="logo" to="/" aria-label="Dogs - Home">
           <Dogs />
         </Link>
-        {data && <Link className="login" to="/login">Login / Criar</Link>}
+        {data && login
+          ? <Link className="login" to="/login">{data.username}</Link>
+          : <Link className="login" to="/login">Login / Criar</Link>
+        }
       </S.Navigation>
     </S.Header>
   )
