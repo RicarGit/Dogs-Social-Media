1f4aada3c265455e2edce072f2e1263772138a80
Ricardo Gava <ricardoagava@gmail.com>
2023-04-01 23:44:19 -0300

    feat: lostPasswordReset done

--- src/Pages/Login/LoginPasswordReset/LoginPasswordReset.styled.ts
+++ src/Pages/Login/LoginPasswordReset/LoginPasswordReset.styled.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components/macro"
 
-export const LoginPasswordReset = styled.div`
+export const LoginPasswordReset = styled.div``
 
-`
+export const PasswordResetTitle = styled.h1``
+
+export const PasswordResetForm = styled.form``
--- src/Pages/Login/LoginPasswordReset/LoginPasswordReset.tsx
+++ src/Pages/Login/LoginPasswordReset/LoginPasswordReset.tsx
@@ -1,7 +1,56 @@
 import * as S from './LoginPasswordReset.styled'
+import { FormEvent, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useForm } from 'hooks/useForm'
+import { useFetch } from 'hooks/useFetch'
+import { api } from 'services/api'
+
+import { FormInput } from 'Components/FormInput'
+import { Button } from 'Components/Button'
+import { ErrorInfo } from 'Components/ErrorInfo'
 
 export const LoginPasswordReset = () => {
+  const [key, setKey] = useState('')
+  const [login, setLogin] = useState('')
+  const password = useForm('password')
+  const { error, loading, request } = useFetch()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    const key = params.get('key')
+    const login = params.get('login')
+
+    if (key) setKey(key)
+    if (login) setLogin(login)
+  }, [])
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+
+    if (password.validate()) {
+      const { url, options } = api.PASSWORD_RESET({ login, key, password: password.value })
+      const { response } = await request(url, options)
+
+      if (response?.ok) navigate('/login')
+    }
+  }
+
   return (
-    <S.LoginPasswordReset>LoginPasswordReset</S.LoginPasswordReset>
+    <S.LoginPasswordReset onSubmit={handleSubmit}>
+      <S.PasswordResetTitle>
+        Reset a Senha
+      </S.PasswordResetTitle>
+
+      <S.PasswordResetForm>
+        <FormInput labelText="Nova Senha" type="password" name="password" {...password} />
+        {loading
+          ? <Button disabled >Resetando...</Button>
+          : <Button>Resetar</Button>
+        }
+      </S.PasswordResetForm>
+
+      <ErrorInfo error={error} />
+    </S.LoginPasswordReset>
   )
 }
