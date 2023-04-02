13eb4865b7d7da86a40b3bbe2be4839a479e13f6
Ricardo Gava <ricardoagava@gmail.com>
2023-04-01 23:40:42 -0300

    feat: loginPasswordLost done

--- src/Pages/Login/LoginPasswordLost/LoginPasswordLost.styled.ts
+++ src/Pages/Login/LoginPasswordLost/LoginPasswordLost.styled.ts
@@ -1,5 +1,11 @@
 import styled from "styled-components/macro"
 
-export const LoginPasswordLost = styled.div`
+export const LoginPasswordLost = styled.section``
 
+export const PasswordLostTitle = styled.h1``
+
+export const PasswordLostForm = styled.form``
+
+export const SendedEmailMessage = styled.p`
+  color: #4c1;
 `
--- src/Pages/Login/LoginPasswordLost/LoginPasswordLost.tsx
+++ src/Pages/Login/LoginPasswordLost/LoginPasswordLost.tsx
@@ -1,7 +1,44 @@
 import * as S from './LoginPasswordLost.styled'
+import { FormEvent } from 'react'
+import { useForm } from 'hooks/useForm'
+import { useFetch } from 'hooks/useFetch'
+import { api } from 'services/api'
+
+import { FormInput } from 'Components/FormInput'
+import { Button } from 'Components/Button'
+import { ErrorInfo } from 'Components/ErrorInfo'
 
 export const LoginPasswordLost = () => {
+  const login = useForm('email')
+  const { data, loading, error, request } = useFetch()
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault()
+    const resetUrl = window.location.href.replace('perdeu', 'resetar')
+
+    if (login.validate()) {
+      const { url, options } = api.PASSWORD_LOST({ login: login.value, url: resetUrl })
+      await request(url, options)
+    }
+  }
+
   return (
-    <S.LoginPasswordLost>LoginPasswordLost</S.LoginPasswordLost>
+    <S.LoginPasswordLost>
+      <S.PasswordLostTitle>
+        Perdeu a senha?
+      </S.PasswordLostTitle>
+      {data
+        ? <S.SendedEmailMessage>Email enviado.</S.SendedEmailMessage>
+        : <S.PasswordLostForm onSubmit={handleSubmit}>
+          <FormInput labelText='Email / Usuário' type='text' name='email' {...login} />
+          {loading
+            ? <Button disabled >Enviando...</Button>
+            : <Button>Enviar Email</Button>
+          }
+        </S.PasswordLostForm>
+      }
+
+      <ErrorInfo error={error} />
+    </S.LoginPasswordLost>
   )
 }
