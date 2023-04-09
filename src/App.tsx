cd44723cb73668419d69e99cc169c857dd407f1b
Ricardo Gava <ricardoagava@gmail.com>
2023-04-09 13:03:24 -0300

    feat: add a container to the App, passing the Main props to other pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,33 +8,41 @@ import { Home, Login, User, NotFound, UserProfile } from "Pages"
 import { Header, Footer, Photo } from "Components"
 import { PrivateRoute } from "Components/PrivateRoute"
 
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: calc(100vh + 6rem);
+`
+
 const Main = styled.main`
-  padding: 0 1rem;
-  max-width: 800px;
-  margin: 2rem auto 0 auto;
+  flex: 1;
 `
 
 export function App() {
   return (
-    <BrowserRouter>
-      <UserContextProvider>
-        <Header />
-        <Main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="login/*" element={<Login />} />
-            <Route path="foto/:id" element={<Photo />} />
-            <Route path="perfil/:user" element={<UserProfile />} />
-            <Route path="conta/*" element={
-              <PrivateRoute>
-                <User />
-              </PrivateRoute>
-            } />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Main>
-        <Footer />
-      </UserContextProvider>
-    </BrowserRouter>
+    <AppContainer>
+      <BrowserRouter>
+        <UserContextProvider>
+          <Header />
+
+          <Main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login/*" element={<Login />} />
+              <Route path="foto/:id" element={<Photo />} />
+              <Route path="perfil/:user" element={<UserProfile />} />
+              <Route path="conta/*" element={
+                <PrivateRoute>
+                  <User />
+                </PrivateRoute>
+              } />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Main>
+
+          <Footer />
+        </UserContextProvider>
+      </BrowserRouter>
+    </AppContainer>
   )
 }
