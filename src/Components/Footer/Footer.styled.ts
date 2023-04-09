da928bc764c209aeff678e7fb28411e48852681c
Ricardo Gava <ricardoagava@gmail.com>
2023-04-08 23:16:32 -0300

    feat: footer done

--- src/Components/Footer/Footer.styled.ts
+++ src/Components/Footer/Footer.styled.ts
@@ -1,5 +1,13 @@
 import styled from "styled-components/macro"
 
 export const Footer = styled.footer`
-  background-color: aquamarine;
+  background-color: #fb1;
+  height: 10rem;
+  padding: 3rem 1rem 0 1rem;
+  text-align: center;
+  color: #764701;
+`
+
+export const FooterCopyright = styled.p`
+  margin-top: 1rem;
 `
--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,9 +1,11 @@
 import * as S from './Footer.styled'
+import { ReactComponent as Dogs } from 'assets/dogs-footer.svg'
 
 export const Footer = () => {
   return (
     <S.Footer>
-      Footer
+      <Dogs />
+      <S.FooterCopyright>Dogs. Alguns direitos reservados.</S.FooterCopyright>
     </S.Footer>
   )
 }
