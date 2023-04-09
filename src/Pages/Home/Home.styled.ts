be098fdbee17c9b580f5fe65142a270a1136b615
Ricardo Gava <ricardoagava@gmail.com>
2023-04-08 23:22:40 -0300

    feat: add styled file to Home page

---
+++ src/Pages/Home/Home.styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components/macro"
+
+export const Home = styled.section`
+  padding: 0 1rem;
+  max-width: 800px;
+  margin: 2rem auto 0 auto;
+`
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,10 +1,11 @@
+import * as S from './Home.styled'
 import { Feed, Head } from "Components"
 
 export const Home = () => {
   return (
-    <section>
+    <S.Home>
       <Head title='Fotos' description='Home do site dogs, com o feed de fotos.' />
       <Feed user={'0'} />
-    </section>
+    </S.Home>
   )
 }
