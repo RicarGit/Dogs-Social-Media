87b4f1d22c21306c03cab76652779f2c3c5ca2de
Ricardo Gava <ricardoagava@gmail.com>
2023-04-09 12:39:51 -0300

    feat: add some styles to act like a container to the User component

--- src/Pages/User/User.styled.ts
+++ src/Pages/User/User.styled.ts
@@ -1,5 +1,7 @@
-import styled from "styled-components"
+import styled from "styled-components/macro"
 
 export const UserSection = styled.section`
-  
+  padding: 0 1rem;
+  max-width: 800px;
+  margin: 2rem auto 0 auto;
 `
