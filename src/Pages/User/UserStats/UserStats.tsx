3c5735dca364fd85f60c18dcd9271b4b5deff757
Ricardo Gava <ricardoagava@gmail.com>
2023-04-11 22:28:52 -0300

    feat: userStats component done

--- src/Pages/User/UserStats/UserStats.tsx
+++ src/Pages/User/UserStats/UserStats.tsx
@@ -1,10 +1,35 @@
-import { Head } from "Components"
+import { useEffect } from "react"
+
+import { api } from "services/api"
+import { useFetch } from "hooks"
+import { getStorageToken } from "contexts/UserContext"
+
+import { ErrorInfo, Head, Loading } from "Components"
+import { UserStatsGraphs } from "./UserStatsGraphs"
 
 export const UserStats = () => {
+  const { data, error, loading, request } = useFetch()
+  const token = getStorageToken()
+
+  useEffect(() => {
+    const getData = async () => {
+      if (token) {
+        const { url, options } = api.STATS_GET(token)
+        await request(url, options)
+      }
+    }
+
+    getData()
+  }, [request, token])
+
+  if (loading) return <Loading />
+  if (error) return <ErrorInfo error={error} />
+  if (!data) return null
+
   return (
     <div>
       <Head title='Estatísticas' />
-      Estatísticas
+      <UserStatsGraphs />
     </div>
   )
 }
