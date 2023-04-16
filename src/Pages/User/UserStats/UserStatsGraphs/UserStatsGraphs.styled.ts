fd0a86d8a72ebc2e7c4f70156d724eee52f0f96c
Ricardo Gava <ricardoagava@gmail.com>
2023-04-16 20:55:56 -0300

    feat: add graphs using victory lib to the UserStatsGraphs component

--- src/Pages/User/UserStats/UserStatsGraphs/UserStatsGraphs.styled.ts
+++ src/Pages/User/UserStats/UserStatsGraphs/UserStatsGraphs.styled.ts
@@ -1,9 +1,29 @@
 import styled from "styled-components/macro"
 
 export const UserStatsGraphs = styled.section`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 2rem;
 
+  .accesses {
+    grid-column: 1/3;
+    padding: 2rem;
+    font-size: 2rem;
+  }
+
+  @media (max-width: 40rem) {
+    grid-template-columns: 1fr;
+    margin-bottom: 2rem;
+  }
 `
 
-export const GraphsTotal = styled.div`
+export const GraphContainer = styled.div`
+  box-shadow: 0 10px 20px rgba(0, 0, 0, .1);
+  border-radius: .2rem;
+  display: grid;
+  align-items: center;
 
+  @media (max-width: 40rem) {
+    grid-column: 1/3;
+  }
 `
--- src/Pages/User/UserStats/UserStatsGraphs/UserStatsGraphs.tsx
+++ src/Pages/User/UserStats/UserStatsGraphs/UserStatsGraphs.tsx
@@ -1,20 +1,78 @@
 import * as S from './UserStatsGraphs.styled'
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { UsersStats } from 'types'
 
-interface UserGraphsInfo {
+import { VictoryPie, VictoryChart, VictoryBar } from 'victory'
+
+export interface UserGraphsInfo {
   data: UsersStats
 }
 
-export const UserStatsGraphs = ({ data }: UserGraphsInfo) => {
-  const [graph, setGraph] = useState([])
+interface GraphData {
+  x: string
+  y: number
+}
+
+const UserStatsGraphs = ({ data }: UserGraphsInfo) => {
+  const [graph, setGraph] = useState<GraphData[]>([])
   const [total, setTotal] = useState(0)
+
+  useEffect(() => {
+    const accesses = data.map(({ acessos }) => Number(acessos))
+    const totalOfAccesses = accesses.reduce((acc, access) => acc + access, 0)
+
+    const graphData = data.map(({ title, acessos }) => (
+      {
+        x: title,
+        y: Number(acessos)
+      }
+    ))
+
+    setTotal(totalOfAccesses)
+    setGraph(graphData)
+  }, [data])
 
   return (
     <S.UserStatsGraphs className='animeLeft'>
-      <S.GraphsTotal>
+      <S.GraphContainer className='accesses'>
         <p>Acessos: {total}</p>
-      </S.GraphsTotal>
+      </S.GraphContainer>
+
+      <S.GraphContainer>
+        <VictoryPie
+          data={graph}
+          innerRadius={70}
+          padAngle={3}
+          padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
+          style={{
+            data: {
+              fillOpacity: .9,
+              stroke: '#fb1',
+              strokeWidth: 2.5
+            },
+            labels: {
+              fontSize: 14,
+              fill: '#fb1'
+            }
+          }}
+        />
+      </S.GraphContainer>
+
+      <S.GraphContainer>
+        <VictoryChart>
+          <VictoryBar
+            data={graph}
+            alignment='start'
+            style={{
+              data: {
+                fill: '#fb1'
+              }
+            }}
+          />
+        </VictoryChart>
+      </S.GraphContainer>
     </S.UserStatsGraphs>
   )
 }
+
+export default UserStatsGraphs
