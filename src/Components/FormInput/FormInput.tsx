c5782dfb979a6cca208300ced36d909d6d6e7a5d
Ricardo Gava <ricardoagava@gmail.com>
2023-05-07 00:02:20 -0300

    refactor: using forwardRef on the custom input to receive the register ref and typed with form types

--- src/Components/FormInput/FormInput.tsx
+++ src/Components/FormInput/FormInput.tsx
@@ -1,31 +1,21 @@
 import * as S from './FormInput.styled'
-import { ChangeEvent } from 'react'
+import { forwardRef } from 'react'
+import { LoginFormType, LoginCreateFormType, FormInputProps, RegisterType, PhotoPostFormType } from 'types/formTypes'
 
-interface Input {
-  name: string
-  labelText: string
-  type: string
-  value: string
-  error: string | null
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void
-  onBlur: () => boolean
-  validate: () => boolean
-}
-
-export const FormInput = ({ name, labelText, type, value, onChange, onBlur, error }: Input) => {
-  return (
-    <S.InputContainer>
-      <S.Label htmlFor={name}>{labelText}</S.Label>
-      <S.Input
-        id={name}
-        name={name}
-        type={type}
-        value={value}
-        onChange={onChange}
-        onBlur={onBlur}
-        required
-      />
-      {error && <S.ErrorMessage>{error}</S.ErrorMessage>}
-    </S.InputContainer>
-  )
-}
+export const FormInput = forwardRef<HTMLInputElement, FormInputProps & RegisterType<LoginFormType | LoginCreateFormType | PhotoPostFormType>>(
+  ({ label, type, error, name, onChange, onBlur }, ref) => {
+    return (
+      <S.InputContainer>
+        <S.Label htmlFor={name}>{label}</S.Label>
+        <S.Input
+          name={name}
+          id={name}
+          type={type}
+          ref={ref}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+        {error && <S.ErrorMessage>{error}</S.ErrorMessage>}
+      </S.InputContainer>
+    )
+  })
