45a152a7e806cbfdf9bdce33bf923d806953302b
Ricardo Gava <ricardoagava@gmail.com>
2023-05-10 03:46:57 -0300

    refactor: loginPasswordReset totally refactored with react-hook-form

--- src/Pages/Login/LoginPasswordReset/LoginPasswordReset.tsx
+++ src/Pages/Login/LoginPasswordReset/LoginPasswordReset.tsx
@@ -1,16 +1,22 @@
-import { FormEvent, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 import { api } from 'services/api'
-import { useForm, useFetch } from 'hooks'
+import { useFetch } from 'hooks'
+import { useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { passwordSchema, FormPasswordType } from 'types'
 
 import { Head, Button, FormInput, ErrorInfo } from 'Components'
 
 export const LoginPasswordReset = () => {
   const [key, setKey] = useState('')
   const [login, setLogin] = useState('')
-  const password = useForm('password')
   const { error, loading, request } = useFetch()
+  const { handleSubmit, register, formState: { errors } } = useForm<FormPasswordType>({
+    resolver: zodResolver(passwordSchema)
+  })
+
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -22,11 +28,9 @@ export const LoginPasswordReset = () => {
     if (login) setLogin(login)
   }, [])
 
-  const handleSubmit = async (event: FormEvent) => {
-    event.preventDefault()
-
-    if (password.validate()) {
-      const { url, options } = api.PASSWORD_RESET({ login, key, password: password.value })
+  const onSubmit = async ({ password }: FormPasswordType) => {
+    if (password) {
+      const { url, options } = api.PASSWORD_RESET({ login, key, password })
       const { response } = await request(url, options)
 
       if (response?.ok) navigate('/login')
@@ -34,16 +38,18 @@ export const LoginPasswordReset = () => {
   }
 
   return (
-    <section onSubmit={handleSubmit}>
+    <section onSubmit={handleSubmit(onSubmit)}>
       <Head title='Resete a Senha' />
       <h1>Resete a Senha</h1>
 
       <form>
-        <FormInput labelText="Nova Senha" type="password" name="password" {...password} />
-        {loading
-          ? <Button disabled >Resetando...</Button>
-          : <Button>Resetar</Button>
-        }
+        <FormInput
+          label="Nova Senha"
+          type="password"
+          error={errors.password?.message}
+          {...register('password')}
+        />
+
       </form>
 
       <ErrorInfo error={error} />
